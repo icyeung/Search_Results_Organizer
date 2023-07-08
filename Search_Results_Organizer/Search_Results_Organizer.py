eea5309cc178b116883e9509a07688ec81411195
isabellayeung <101486851+isabellayeung@users.noreply.github.com>
2023-07-07 22:26:00 -0400

    buggy

--- Search_Results_Organizer/Search_Results_Organizer.py
+++ Search_Results_Organizer/Search_Results_Organizer.py
@@ -10,9 +10,13 @@ numofLines = 0
 
 searchTerm1 = []
 searchTerm2 = []
+commonResults = []
 
-with open(os.path.join(__location__, 'Factiva & ProQuest Comparison (1).csv'), 'r', errors='ignore') as csvfile:
-    #file = open(csvfile, encoding="utf8")
+
+searchTerm1DR = []
+searchTerm2DR = []
+
+with open(os.path.join(__location__, 'TesterFile.csv'), 'r', encoding = "utf8") as csvfile:
     lines = csv.reader(csvfile, delimiter = ',')
     for row in lines:
         if not row[0] == "" and numofLines > 0:
@@ -24,13 +28,34 @@ with open(os.path.join(__location__, 'Factiva & ProQuest Comparison (1).csv'), '
         else:
             numofLines += 1
 
-for result2 in searchTerm2:
-    for result1 in searchTerm1:
-        if result1 == result2:
-            searchTerm2.remove(result1)
-            searchTerm1.remove(result1)
+print("Number of Results for Search Term 1 Originally: ", len(searchTerm1))
+print("Number of Results for Search Term 2 Originally: ", len(searchTerm2))
 
-searchFilteredDF = pd.DataFrame({'sea grant AND acifification (with quotations)': pd.Series(searchTerm1), 'sea grant ocean acidification': pd.Series(searchTerm2)})
+for result in searchTerm1:
+    if result not in searchTerm1DR:
+        searchTerm1DR.append(result)
+
+print(searchTerm1DR)
+
+for result in searchTerm2:
+    if result not in searchTerm2DR:
+        searchTerm2DR.append(result)
+
+print(searchTerm2DR)
+
+print("Number of Results for Search Term 1 After Duplicates Removed: ", len(searchTerm1DR))
+print("Number of Results for Search Term 2 After Duplicates Removed: ", len(searchTerm2DR))
+
+for result1 in searchTerm1DR:
+    for result2 in searchTerm2DR:
+        if result1 == result2:
+            print(result1)
+            commonResults.append(result1)
+            searchTerm2DR.remove(result1)
+            searchTerm1DR.remove(result1)
+            
+
+searchFilteredDF = pd.DataFrame({'sea grant AND acifification (with quotations)': pd.Series(searchTerm1DR), 'sea grant ocean acidification': pd.Series(searchTerm2DR), 'Results in Common': pd.Series(commonResults)})
 
 searchFilteredDF.to_excel("Test.xlsx", index=False)
 
